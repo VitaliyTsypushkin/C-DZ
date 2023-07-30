4faceae58daae3ce5176e75c67dbea1d1e75bafe
VitalyTsyp <vitalytsyp@mail.ru>
2023-07-30 15:06:48 +0500

    added T41

--- DZ4/Program.cs
+++ DZ4/Program.cs
@@ -30,3 +30,24 @@
 // Console.WriteLine($"Сумма цифр числа = {sum}");
 /////////////////////////////////////////////////
 //Задача 29
+// //задать массив из 8 элементов и вывести на экран сначала 5 элементов и ниже 3 элемента
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 50);
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var item in array)
+        Console.Write($"{item} ");
+    System.Console.WriteLine();
+}
+
+
+Console.Clear();
+System.Console.WriteLine("Введите размерность массива ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+int [] array = new int[num];
+FillArray(array);
+PrintArray(array);
